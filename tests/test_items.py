aceb3975602b10c1c660588b913e091bdc607a8e
Nathan Spiegel <nathan@zswap.net>
2021-12-23 11:53:52 -0500

    does it work from here

--- tests/test_items.py
+++ tests/test_items.py
@@ -5,7 +5,7 @@ from emcommon.items import get_typeName
 import redis
 
 
-r = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis(host='redis-master', port=6379, db=0)
 
 def test_get_typeName():
     assert get_typeName(34) == 'Tritanium'
