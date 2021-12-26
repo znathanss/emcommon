f411dbd823e315c42d47acc640c0787e1b12eb54
Nathan Spiegel <nathan@zswap.net>
2021-12-26 15:22:14 -0500

    see if that builds without line breaks

--- src/emcommon/items.py
+++ src/emcommon/items.py
@@ -20,14 +20,12 @@ class Item:
     def info(self, field):
         """ Return the typeName for a given type_id"""
         if not self.redis_conn:
-            request_url = f"https://esi.evetech.net/latest/\
-            universe/types/{self.type_id}/?datasource=tranquility&language=en"
+            request_url = f"https://esi.evetech.net/latest/universe/types/{self.type_id}/?datasource=tranquility&language=en"
             data = json.loads(esi_request(request_url, "GET"))
             return data[field]
         cached_data = self.redis_conn.get(f'type_info_{self.type_id}')
         if cached_data is None:
-            request_url = f"https://esi.evetech.net/latest/\
-            universe/types/{self.type_id}/?datasource=tranquility&language=en"
+            request_url = f"https://esi.evetech.net/latest/universe/types/{self.type_id}/?datasource=tranquility&language=en"
             data = json.loads(esi_request(request_url, "GET"))
             self.redis_conn.setex(f'type_info_{self.type_id}', 3600, json.dumps(data))
             return data[field]
@@ -37,17 +35,14 @@ class Item:
     def orders(self, system_id=None):
         """ Return a list of orders for this type_id"""
         if system_id is None:
-            request_url = f"https://api.eve-market.net/\
-            orders?item_id={self.type_id}"
+            request_url = f"https://api.eve-market.net/orders?item_id={self.type_id}"
         else:
-            request_url = f"https://api.eve-market.net/\
-            orders?item_id={self.type_id}&system_id={system_id}"
+            request_url = f"https://api.eve-market.net/orders?item_id={self.type_id}&system_id={system_id}"
         data = json.loads(requests.get(request_url).text)
         return data
 
     def price(self, system_id=30000142):
         """ Return the price for this type_id. Defaults to Jita """
-        request_url = f"https://api.eve-market.net/\
-        get_adjusted_market_price?type_id={self.type_id}&market={system_id}"
+        request_url = f"https://api.eve-market.net/get_adjusted_market_price?type_id={self.type_id}&market={system_id}"
         data = json.loads(requests.get(request_url).text)
         return data
